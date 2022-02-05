b3476c17bdb58d8f208a8fb622c83d3a4a098e7c
wissensalt <wissensalt@gmail.com>
2022-02-05 18:25:03 +0700

    todo: need to fix sink

--- deploy-configurations.sh
+++ deploy-configurations.sh
@@ -3,5 +3,5 @@
 # setup source connector
 curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/debezium-kf_ppob-source-connector.json
 
-# setup source connector
-#curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/debezium-kf_ppob-sink-connector.json
+# setup sink connector
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @configs/debezium-kf_ppob-sink-connector.json
